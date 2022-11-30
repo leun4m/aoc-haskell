44ae410225c606dda546edf931d8b7274278eeca
Manuel Krebs <me@leun4m.de>
2022-11-30 19:28:08 +0100

    Initial commit

---
+++ day_01.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- Day 1 / 2020
+
+import Debug.Trace
+import System.IO
+
+main :: IO ()
+main = do
+  input <- getContents
+  let parsed = parse input
+  print (solve1 parsed)
+  print (solve2 parsed)
+
+parse :: String -> [Integer]
+parse x = map read (lines x) :: [Integer]
+
+solve1 :: [Integer] -> Integer
+solve1 (x : y : rest) = (if x < y then 1 else 0) + solve1 (y : rest)
+solve1 _ = 0
+
+solve2 :: [Integer] -> Integer
+solve2 (w : x : y : z : rest) = (if w < z then 1 else 0) + solve2 (x : y : z : rest)
+solve2 _ = 0
