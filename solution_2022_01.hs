8d2836ba4ddb68f013ab8e1107258145c3bc1d5d
Manuel Krebs <me@leun4m.de>
2022-12-01 13:17:34 +0100

    2022 / Day 01 - Part 2

--- solution_2022_01.hs
+++ solution_2022_01.hs
@@ -2,10 +2,11 @@
 
 -- Day 1 / 2022
 
+import Data.List (sort)
+import Data.Maybe
 import Data.Text (Text, pack, splitOn, unpack)
 import GHC.Real (reduce)
 import System.IO
-import Data.Maybe
 import Text.Read
 
 main :: IO ()
@@ -13,6 +14,7 @@ main = do
   input <- getContents
   let parsed = parse input
   print (solve1 parsed)
+  print (solve2 parsed)
 
 parse :: String -> [[Int]]
 parse input = map parseGroup (splitOn "\n\n" (pack input))
@@ -22,3 +24,8 @@ parseGroup group = mapMaybe (readMaybe . unpack) (splitOn "\n" group)
 
 solve1 :: [[Int]] -> Int
 solve1 elves = maximum (map sum elves)
+
+solve2 :: [[Int]] -> Int
+solve2 elves = do
+  let (a : b : c : xs) = reverse (sort (map sum elves))
+  a + b + c
