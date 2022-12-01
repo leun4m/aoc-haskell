8d3b0904f9528e72804c8ade4b5b7543efaf9ff6
Manuel Krebs <me@leun4m.de>
2022-12-01 18:46:37 +0100

    Compress Tests

--- solution_2022_01.hs
+++ solution_2022_01.hs
@@ -4,10 +4,10 @@
 -- Day 1 / 2022
 
 import Data.List (sort)
-import Data.Maybe ( mapMaybe )
+import Data.Maybe (mapMaybe)
 import Data.Text (Text, pack, splitOn, unpack)
-import Test.HUnit ( assertEqual, Test(..) )
-import Text.Read ( readMaybe )
+import Test.HUnit
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
@@ -36,16 +36,10 @@ exampleInput = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10
 exampleList :: [[Int]]
 exampleList = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
 
-testParse :: Test
-testParse = TestCase (assertEqual "Example" exampleList (parse exampleInput))
-
-testPart1 :: Test
-testPart1 = TestCase (assertEqual "Example" 24000 (solve1 exampleList))
-
-testPart2 :: Test
-testPart2 = TestCase (assertEqual "Example" 45000 (solve2 exampleList))
-
-tests = TestList [
-  TestLabel "Parse" testParse, 
-  TestLabel "Part 1" testPart1,
-  TestLabel "Part 2" testPart2]
+tests :: Test
+tests =
+  TestList
+    [ TestCase (assertEqual "Parse Example" exampleList (parse exampleInput)),
+      TestCase (assertEqual "Part1 Example" 24000 (solve1 exampleList)),
+      TestCase (assertEqual "Part2 Example" 45000 (solve2 exampleList))
+    ]
