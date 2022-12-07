381ec3d44a4247efb6fa2f764a94446c6fc2edf4
Manuel Krebs <me@leun4m.de>
2022-12-07 14:57:08 +0100

    2022 / Day 5 (Part 1)

--- solution_2022_05.hs
+++ solution_2022_05.hs
@@ -1,17 +1,18 @@
 {-# OPTIONS -Wunused-imports #-}
+-- {-# OPTIONS -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- Day 4 / 2022
 
 import Data.Text (Text, pack, replace, splitOn, unpack)
 import Test.HUnit
-import Debug.Trace ( trace )
 
 main :: IO ()
 main = do
   input <- getContents
   let parsed = parse input
   print (solve1 parsed)
+
 -- print (solve2 parsed)
 
 type Stack = [Char]
@@ -22,65 +23,55 @@ type Output = ([Stack], [Instruction])
 
 parse :: String -> Output
 parse input = do
-  let (a : b : _) = splitOn "\n\n" (pack input)
-  let (m : _) = splitOn "1" a
-  (parseStacks m, parseInstructions b)
+  let (stacksAndNumbers : instructions : _) = splitOn "\n\n" (pack input)
+  let (stacks_ : _) = splitOn "1" stacksAndNumbers
+  (parseStacks stacks_, parseInstructions instructions)
 
 parseStacks :: Text -> [Stack]
-parseStacks w | trace ("parseStacks: " ++ show w) False = undefined
-parseStacks m = map (unpack . replace "-" "" . pack) (transposeString (filter (not . null) (map (replaceAllUnnecessary . unpack) (splitOn "\n" m))))
+-- parseStacks x | trace ("parseStacks: " ++ show x) False = undefined
+parseStacks x = map (unpack . replace "-" "" . pack) (transposeString (filter (not . null) (map processStackLine (lines (unpack x)))))
 
 parseInstructions :: Text -> [Instruction]
-parseInstructions w | trace ("parseInstructions: " ++ show w) False = undefined
-parseInstructions b = map mapInstruction (filter (not . null . unpack) (splitOn "\n" b))
+-- parseInstructions x | trace ("parseInstructions: " ++ show x) False = undefined
+parseInstructions x = map parseInstruction (filter (not . null . unpack) (splitOn "\n" x))
 
-mapInstruction :: Text -> Instruction
-mapInstruction w | trace ("mapInstruction " ++ show w) False = undefined
-mapInstruction w = do
-  let (x : y : z : _) = filter (not . null) (map unpack (splitOn "/" (replace " " "" (replace "move" "/" (replace "from" "/" (replace "to" "/" w))))))
-  (stripAndParse x, stripAndParse y, stripAndParse z)
+parseInstruction :: Text -> Instruction
+-- parseInstruction x | trace ("parseInstruction: " ++ show x) False = undefined
+parseInstruction x = do
+  let (move : from : to : _) = filter (not . null) (map unpack (splitOn "/" (replace "move" "/" (replace "from" "/" (replace "to" "/" x)))))
+  (stripAndParse move, stripAndParse from, stripAndParse to)
 
 stripAndParse :: String -> Int
-stripAndParse x = read (strip x)
+stripAndParse x = read (filter (/= ' ') x)
 
-strip :: String -> String
-strip x = unpack (replace " " "" (pack x))
-
-replaceAllUnnecessary :: String -> String
-replaceAllUnnecessary t = unpack (removeBrackes (replace "    " "-" (pack t)))
-
-removeBrackes :: Text -> Text
-removeBrackes x = replace "[" "" (replace "]" "" (replace " " "" x))
+processStackLine :: String -> String
+processStackLine x = filter (`notElem` ['[', ']', ' ']) (unpack (replace "    " "-" (pack x)))
 
 -- https://stackoverflow.com/questions/36962520/haskell-transposition-on-a-list-of-strings
 transposeString :: [String] -> [String]
 transposeString ([] : _) = []
-transposeString x | trace ("transpose: " ++ show x) False = undefined
+-- transposeString x | trace ("transpose: " ++ show x) False = undefined
 transposeString x = map head x : transposeString (map tail x)
 
-partition :: Int -> [a] -> [[a]]
-partition _ [] = []
-partition n xs = take n xs : partition n (drop n xs)
-
 solve1 :: Output -> String
-solve1 (x,y) | trace ("solve1" ++ show x) False = undefined
+-- solve1 x | trace ("solve1" ++ show x) False = undefined
 solve1 x = concatMap (take 1) (applyInstructions x)
 
 applyInstructions :: Output -> [Stack]
 applyInstructions (s, []) = s
-applyInstructions (s, x : xs) = applyInstructions (applyI s x, xs)
+applyInstructions (s, x : xs) = applyInstructions (applyInstruction s x, xs)
 
-applyI :: [Stack] -> Instruction -> [Stack]
-applyI s (0, y, z) = s
-applyI s (x, y, z) = do
-  let elem = take 1 (s !! (y - 1))
-  let m = zipWith (\i s -> switchIt (i, s) y z elem) [1 ..] s
-  applyI m (x -1, y, z)
+applyInstruction :: [Stack] -> Instruction -> [Stack]
+applyInstruction w (0, _, _) = w
+applyInstruction w (x, y, z) = do
+  let item = take 1 (w !! (y - 1))
+  let newStack = zipWith (\i s -> switchIt (i, s) y z item) [1 ..] w
+  applyInstruction newStack (x -1, y, z)
 
 switchIt :: (Int, Stack) -> Int -> Int -> [Char] -> Stack
-switchIt (i, s) y z elem
+switchIt (i, s) y z item
   | i == y = drop 1 s
-  | i == z = elem ++ s
+  | i == z = item ++ s
   | otherwise = s
 
 -- Tests
